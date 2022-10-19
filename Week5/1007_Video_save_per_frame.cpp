833efc0dccf0438b0f4609fceb8e98f0ee1ab522
JaeHyeon Lee <wogusn@gmail.com>
2022-10-19 21:48:53 +0900

    Rename 1007_Video_save_per_frame.cpp to Week5/1007_Video_save_per_frame.cpp

--- Week5/1007_Video_save_per_frame.cpp
+++ Week5/1007_Video_save_per_frame.cpp
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-// 2022. 10. 07. ภüฑโภüภฺฐ๘วะบฮ ภฬภ็ว๖
+// 2022. 10. 07. ร€รผยฑรขร€รผร€รยฐรธรรยบร ร€รร€รงรรถ
 
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
